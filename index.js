4ed7fcfe98c64caf6269671f78a135a5bdb9c6ed
Andrew Gordienko <50226581+ittrainbow@users.noreply.github.com>
2023-08-26 22:42:55 +0300

    first deploy

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const path = require('path')
 const express = require('express')
 const http = require('http')
 const socket = require('socket.io')
@@ -18,7 +17,6 @@ const getRooms = () => {
 
   // get uuid created rooms only cause we do not need auto-created rooms while they're present by default
   const filteredRooms = Array.from(rooms.keys()).filter((roomID) => validateRoomID(roomID))
-  
   return filteredRooms
 }
 
@@ -30,43 +28,14 @@ const shareRooms = () => {
 }
 
 io.on('connection', (socket) => {
-  console.log('Socket connected')
   shareRooms()
-
-  io.on('JOIN_ROOM', (config) => {
-    const { room: roomID } = config
-    const { rooms } = socket
-
-    if (Array.from(rooms).includes(room)) {
-      return console.error('already connected to room')
-    }
-
-    // get all peers connected to this room or empty array if noone
-    const clients = Array.from(io.sockets.adapter.rooms.get(roomID) | [])
-
-    clients.forEach((clientID) => {
-      // tell every peer in room to add new joining peer, no offer created
-      io.to(clientID).emit('ADD_PEER', {
-        peerID: socket.id,
-        createOffer: false
-      })
-
-      // tell joining peer to add every other client id and create an offer
-      socket.emit('ADD_PEER', {
-        peerID: clientID,
-        createOffer: true
-      })
-    })
-
-    socket.join(roomID)
-    shareRooms()
-  })
+  console.log('Socket connected')
 
   const leaveRoom = () => {
     const { rooms } = socket
     Array.from(rooms).forEach((roomID) => {
       // get all peers connected to this room or empty array if noone
-      const clients = Array.from(io.sicket.adapter.rooms.get(roomID) || [])
+      const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || [])
 
       clients.forEach((client) => {
         // tell every peer in room to disconnect from leaving peer
@@ -80,13 +49,65 @@ io.on('connection', (socket) => {
         })
       })
 
+      console.log(`peer ${socket.id} leaving room ${roomID}`)
       socket.leave(roomID)
+      shareRooms()
     })
-
-    // same func for both socket actions
-    socket.on('LEAVE_ROOM', leaveRoom)
-    socket.on('disconnecting', leaveRoom)
   }
+
+  const joinRoom = (data) => {
+    {
+      const { roomID } = data
+      const { rooms } = socket
+
+      if (Array.from(rooms).includes(roomID)) {
+        return console.error('already connected to room')
+      }
+
+      // get all peers connected to this room or empty array if noone
+      const clients = Array.from(io.sockets.adapter.rooms.get(roomID) | [])
+
+      clients.forEach((clientID) => {
+        // tell every peer in room to add new joining peer, no offer created
+        io.to(clientID).emit('ADD_PEER', {
+          peerID: socket.id,
+          shouldCreateOffer: false
+        })
+
+        // tell joining peer to add every other client id and create an offer
+        socket.emit('ADD_PEER', {
+          peerID: clientID,
+          shouldCreateOffer: true
+        })
+      })
+
+      console.log(`peer ${socket.id} joined room ${roomID}`)
+      socket.join(roomID)
+      shareRooms()
+    }
+  }
+
+  const sessionDescription = ({ peerID, sessionDescription }) => {
+    // send from current peer
+    io.to(peerID).emit('SESSION_DESCIPTION', {
+      peerID: socket.id,
+      sessionDescription
+    })
+  }
+
+  const iceCandidate = ({ peerID, iceCandidate }) => {
+    // send from current peer
+    io.to(peerID).emit('ICE_CANDIDATE', {
+      peerID: socket.id,
+      iceCandidate
+    })
+  }
+
+  socket.on('JOIN_ROOM', joinRoom)
+  socket.on('LEAVE_ROOM', (socket) => leaveRoom(socket))
+  socket.on('disconnecting', (socket) => leaveRoom(socket))
+  socket.on('RELAY_LOCALDESC', sessionDescription)
+  socket.on('RELAY_ICECANDIDATE', iceCandidate)
 })
 
 server.listen(PORT, () => console.log(`Server is running at ${PORT}`))
